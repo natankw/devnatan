0a00364a18f54f960fa22a6b8e83c25f0b7b1a0e
natankw <hll931200@gmail.com>
2026-07-24 13:08:20 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+/* ==========================
+        R.H.S CONFIG
+========================== */
+
+
+const CONFIG = {
+
+
+nome:"R.H.S",
+
+
+canalOficial:"#",
+
+
+logo:"img/logo.png",
+
+
+musica:"audio/music.mp3",
+
+
+versao:"V4"
+
+
+};
